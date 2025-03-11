2c6c304d0bd33548fdb4f0bbc3677fb70095e1cc
chandanch <chandan.cec107@gmail.com>
2025-03-11 23:06:19 +0530

    Refactor button class management and add tests for button state and kebab case conversion

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import { kebabCaseToTitleCase } from "./helpers";
 
 test("button click flow", () => {
   render(<App />);
@@ -47,4 +48,42 @@ test("checkbox flow", () => {
   fireEvent.click(checkboxElement);
   expect(buttonElement).toBeEnabled();
   expect(buttonElement).toHaveClass("red");
+});
+
+test("checkbox flow after button click", () => {
+  // render app
+  render(<App />);
+
+  // find elements
+  const buttonElement = screen.getByRole("button", {
+    name: /blue/i,
+  });
+  const checkboxElement = screen.getByRole("checkbox", {
+    name: /disable button/i,
+  });
+
+  // click button to change to blue
+  fireEvent.click(buttonElement);
+
+  // click checkbox to disable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeDisabled();
+  expect(buttonElement).toHaveClass("gray");
+
+  // click checkbox to re-enable button
+  fireEvent.click(checkboxElement);
+  expect(buttonElement).toBeEnabled();
+  expect(buttonElement).toHaveClass("blue");
+});
+
+describe("kebabCaseToTitleCase", () => {
+  test("Works for no hypens", () => {
+    expect(kebabCaseToTitleCase("red")).toBe("Red");
+  });
+  test("Works for one hyphen", () => {
+    expect(kebabCaseToTitleCase("midnight-blue")).toBe("Midnight Blue");
+  });
+  test("Works for multiple inner hyphens", () => {
+    expect(kebabCaseToTitleCase("medium-violet-red")).toBe("Medium Violet Red");
+  });
 });
